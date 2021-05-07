biocryst/biozernike

commit c349925b5e2b73ab652670eb2dd09ae442ddf716

Now converting to positive outside the parser

src/main/java/org/rcsb/biozernike/volume/Volume.java
@@ -493,6 +493,18 @@ public class Volume {
 		applyContourAndNormalize(stdDev * stdDevMultiplier, multiplier);
 	}
 
+	/**
+	 * Convert all density values to positive (taking absolute value).
+	 * Note that EMDB maps usually have negative values. This will convert those to positive, which is better suited for
+	 * BioZernike descriptors calculation, especially when comparing to volumes that are calculated from
+	 * coordinates (where all density is positive).
+	 */
+	public void positivize() {
+		for (int i = 0; i<voxelArray.length; i++) {
+			voxelArray[i] *= Math.abs(voxelArray[i]);
+		}
+	}
+
 	public DescriptiveStatistics getDescriptiveStatistics() {
 		return new DescriptiveStatistics(voxelArray);
 	}

src/main/java/org/rcsb/biozernike/volume/VolumeIO.java
@@ -309,7 +309,7 @@ public class VolumeIO {
 					else if (mode==1)
 						val = (float)dis.readShort();
 					else if (mode==2)
-						val = Math.abs(reversedFloat(dis));
+						val = reversedFloat(dis);
 
 					voxels[(z*dim + y)*dim + x] = val;
 				}

src/test/java/org/rcsb/biozernike/DescriptorTest.java
@@ -143,6 +143,7 @@ public class DescriptorTest {
 		// some hardcoded scaling coefficients for the EM volume (as we do not control the density values)
 		InputStream is = DescriptorTest.class.getResourceAsStream("/emd_3186.map");
 		Volume volumeEM = VolumeIO.read(is, MapFileType.MRC);
+		volumeEM.positivize();
 		volumeEM.applyContourAndNormalize(0.0176, 757);
 		volumeEM.setRadiusVarMult(1.64);
 

commit 04fb209c6058aef6352a32130e929f8f67d5703e

Fixing bug introduced in c349925

src/main/java/org/rcsb/biozernike/volume/Volume.java
@@ -463,7 +463,6 @@ public class Volume {
 		int nVoxels = 0;
 
 		for (int i = 0; i<voxelArray.length; i++) {
-
 			if(voxelArray[i] <= contourThreshold) {
 				voxelArray[i] = 0;
 				continue;
@@ -474,7 +473,6 @@ public class Volume {
 		}
 
 		double normCoef = nVoxels*multiplier/sumval;
-
 		for (int i = 0; i<voxelArray.length; i++) {
 			voxelArray[i] *= normCoef;
 		}
@@ -501,8 +499,9 @@ public class Volume {
 	 */
 	public void positivize() {
 		for (int i = 0; i<voxelArray.length; i++) {
-			voxelArray[i] *= Math.abs(voxelArray[i]);
+			voxelArray[i] = Math.abs(voxelArray[i]);
 		}
+		updateCenter();
 	}
 
 	public DescriptiveStatistics getDescriptiveStatistics() {