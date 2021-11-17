5966c6373481720b50d11fed8d0f3736b2452bdd
Mark Williamson <mjw@mjw.name>
2021-11-17 19:01:01 +0000

    Make test use less memory

--- src/test/java/org/rcsb/biozernike/InvariantNormTest.java
+++ src/test/java/org/rcsb/biozernike/InvariantNormTest.java
@@ -15,7 +15,7 @@ public class InvariantNormTest {
 
 	@Test
 	public void invariants() {
-		int nPoints = 50;
+		int nPoints = 10;
 		double coordsLimit = 96;
 		Point3d[] points = new Point3d[nPoints];
 
